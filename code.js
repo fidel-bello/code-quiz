f5007cef51dca8643105bd37c2ab6e00104bcc02
fidel bello <59423@bears.smcc.edu>
2021-01-14 16:43:19 -0500

    got elements by id in the dom

---
+++ code.js
@@ -0,0 +1,22 @@
+//used get element by id method to manipulate
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("endQuiz");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("start");
+var highscoreContainer = document.getElementById("highscore");
+var highscoreDiv = document.getElementById("highScorePage");
+var highscoreInputName = document.getElementById("initials");
+var highscoreDisplayName = document.getElementById("highScoreInitials");
+var endGameBtns = document.getElementById("endGame");
+var submitScoreBtn = document.getElementById("submitScore");
+var highscoreDisplayScore = document.getElementById("score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+
