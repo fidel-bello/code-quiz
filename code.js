dfc5cef4ca061b481e229481664b37aa5389ee95
fidel bello <59423@bears.smcc.edu>
2021-01-14 23:06:16 -0500

    made arrays for the questions

--- code.js
+++ code.js
@@ -19,13 +19,40 @@ var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
+
 var questions = [{
     question : "The console.log method is used for __.",
     answerA : "Looping",
     answerB : "Debugging",
     answerC: "Event Listening",
-    answerD: "implementing css"},
+    answerD: "implementing css",
+    correctAnswer: "b"},
     {
-        question: ""
-    }
+        question: "Javascript is a __language.",
+        answerA: "foreign",
+        answerB: "Html",
+        answerC: "programming",
+        answerD: "arithmatic",
+        correctAnswer: "c"},
+        {
+            question: "Strings can be used in __",
+            answerA: "double or single quotes",
+            answerB: "brackets",
+            answerC: "comments",
+            answerD: "parenthesis",
+            correctAnswer: "a"},
+        {
+            question: "Math.Random with math.floor can be be used to retun a random ___",
+            answerA: "letter",
+            answerB: "function",
+            answerC: "variable",
+            answerD: "integer",
+            correctAnswer: "d"},
+            {
+            question: "Use if to specify a block of code to be executed if specified conditions is __.",
+            answerA: "true",
+            answerB: "false",
+            answerC: "fake",
+            answerD: "real",
+            correctAnswer: "a"},
     ]
